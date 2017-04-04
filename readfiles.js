990b9a64ec9a545eaa7f16726558bfadac2c9ae1
schalk <schalk@users.noreply.github.com>
2017-04-04 15:45:58 -0400

    Update readfiles.js

--- readfiles.js
+++ readfiles.js
@@ -1,1 +1,20 @@
-var dir = require('node-dir');function getFiles(callback) {var fileMap = new Map();dir.readFiles(__dirname + '/data', {    match: /.json$/,    shortName: true,    exclude: /^\./    }, function(err, content, filename, next, callback) {        if (err) throw err;        fileMap.set(filename, content);        next();    },    function(err) {      callback(fileMap);    });}getFiles(function(map) {  for(var [key, value] of map.entries()) {    console.log(key + ": " + value);  }});
+var dir = require('node-dir');
+function getFiles(callback) {
+  var fileMap = new Map();
+  dir.readFiles(__dirname + '/data', {    
+    match: /.json$/,    
+    shortName: true,    
+    exclude: /^\./    
+  }, function(err, content, filename, next, callback) {        
+    if (err) throw err;        
+    fileMap.set(filename, content);        
+    next();    
+  },    
+  function(err) {      
+    callback(fileMap);    
+  });}
+getFiles(function(map) {  
+  for(var [key, value] of map.entries()) {    
+    console.log(key + ": " + value);  
+  }
+});
